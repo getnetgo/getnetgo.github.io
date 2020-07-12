0bfd33f44dbeff4df69909b78a56a53d80a9b4bf
getnetgo <55332595+getnetgo@users.noreply.github.com>
2020-07-13 02:02:18 +0530

    #1

--- js/main.js
+++ js/main.js
@@ -240,7 +240,7 @@ function reply_click(element)
 
 //<![CDATA[
 		$(window).on('load', function() { // makes sure the whole site is loaded 
-			$('#status').fadeOut(); // will first fade out the loading animation 
+			$('#loader').fadeOut(); // will first fade out the loading animation 
             $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website. 
             $('body').delay(350).css({'overflow':'visible'});
 		})
